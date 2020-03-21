acd819eb4a718d09c081f10733832803e9910e27
Cee Jay <52139555+Somto811@users.noreply.github.com>
2020-03-21 12:49:22 +0000

    Update clk.py

--- xtrabot/modules/clk.py
+++ xtrabot/modules/clk.py
@@ -13,7 +13,7 @@ async def enaclk(event):
         user=person.first_name
         rantext = ''.join(random.choice(string.ascii_lowercase + string.digits) for _ in range(7))
         api_token = '32f9105a8194b5596482f0ed9631ab1483ec171e'
-        req = requests.get('https://ilinkshort.com/api?api={}&url={}&alias={}'.format(api_token, m.text, rantext)).json()
+        req = requests.get('https://urlshortx.com//api?api={}&url={}&alias={}'.format(api_token, m.text, rantext)).json()
         if(req["status"] == 'error'):
           smsg = req["message"]
         else:
